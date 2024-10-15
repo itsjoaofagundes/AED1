aa6b5df3869a55ee28be2634811c6fc8672715c6
itsjoaofagundes <joao.fagundes07@unifesp.br>
2024-10-15 19:25:54 -0300

    Create 1002.c

---
+++ Contests/1002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+    double raio, area, pi = 3.14159;
+    
+    scanf("%lf", &raio);
+    
+    area = pi * pow(raio, 2);
+    
+    printf("A=%.4lf\n", area);
+ 
+    return 0;
+}
